8ee183f6aa5f1b46d15fd3864d6778b6bb99eac1
wmentzer <willmentzer20@gmail.com>
2020-03-22 01:07:51 -0700

    Created 2 homepage options

--- GoGoGnomes/homepage/views.py
+++ GoGoGnomes/homepage/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, Welcome to the home page of the Go Go Gnomes!")
+    #return HttpResponse("Hello, Welcome to the home page of the Go Go Gnomes!")
+    return render(request, "rootpage2.html")
