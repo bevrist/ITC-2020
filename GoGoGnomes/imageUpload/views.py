ee12a4b22670175fe0ed5e3917e1c1b41cfa2839
equati0n <tr14rc3@gmail.com>
2020-03-28 10:54:26 -0700

    Add Python Code for Upload Page. Add Requirement Pillows
    Added code for the upload page to create 5 cards for the christmas selection. Add pillow to requirements text file.

--- GoGoGnomes/imageUpload/views.py
+++ GoGoGnomes/imageUpload/views.py
@@ -3,7 +3,6 @@ from django.core.files.storage import FileSystemStorage
 from PIL import Image
 
 def imageUpload(request):
-
     if request.method == 'GET':
         print('GET')
 
@@ -16,12 +15,27 @@ def imageUpload(request):
         fs = FileSystemStorage()
         fs.save(uploaded_image.name, uploaded_image)
         print('SAVED')
+        if selection == "christmas":           
+            foreground = Image.open(uploaded_image).convert("RGBA").resize((950,550))
+            background = Image.open("/workspace/GoGoGnomes/static/images/Christmas/c1Blank.png").convert("RGBA")
+            background.paste(foreground, (440,190), foreground)
+            background.save("./media/new-image1.png")
 
-    foreground = Image.open(uploaded_image).convert("RGBA").resize((1050,480))
-    background = Image.open("/workspace/GoGoGnomes/static/images/c1.png").convert("RGBA")
+            background = Image.open("/workspace/GoGoGnomes/static/images/Christmas/c2Blank.png").convert("RGBA")
+            background.paste(foreground, (440,190), foreground)
+            background.save("./media/new-image2.png")
 
-    print("GRABBED")
+            background = Image.open("/workspace/GoGoGnomes/static/images/Christmas/c3Blank.png").convert("RGBA")
+            background.paste(foreground, (440,190), foreground)
+            background.save("./media/new-image3.png")
 
-    background.paste(foreground, (437,213), foreground)
-    background.save("/workspace/GoGoGnomes/media/new-image.png")
+            background = Image.open("/workspace/GoGoGnomes/static/images/Christmas/c4Blank.png").convert("RGBA")
+            background.paste(foreground, (440,190), foreground)
+            background.save("./media/new-image4.png")
+
+            background = Image.open("/workspace/GoGoGnomes/static/images/Christmas/c5Blank.png").convert("RGBA")
+            background.paste(foreground, (440,190), foreground)
+            background.save("./media/new-image5.png")
+
+    print("GRABBED")    
     return render(request, 'uploadpage.html')
