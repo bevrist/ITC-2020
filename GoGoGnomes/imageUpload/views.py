57724491857e708beb5b6f7412f6226e19580406
equati0n <tr14rc3@gmail.com>
2020-04-02 07:16:32 -0700

    Add Text html

--- GoGoGnomes/imageUpload/views.py
+++ GoGoGnomes/imageUpload/views.py
@@ -26,21 +26,21 @@ def imageUpload(request):
         '''if selection == "christmas":           
             foreground = Image.open(uploaded_image).convert("RGBA").resize((950,550))
             background = Image.open("/workspace/GoGoGnomes/static/images/Christmas/c1Blank.png").convert("RGBA")
-            background.paste(foreground, (440,190), foreground)
+            background.paste(foreground, (440,190), foreground)            
             background.save("./media/new-image1.png")
-
+            
             background = Image.open("/workspace/GoGoGnomes/static/images/Christmas/c2Blank.png").convert("RGBA")
             background.paste(foreground, (440,190), foreground)
             background.save("./media/new-image2.png")
-
+            
             background = Image.open("/workspace/GoGoGnomes/static/images/Christmas/c3Blank.png").convert("RGBA")
             background.paste(foreground, (440,190), foreground)
             background.save("./media/new-image3.png")
-
+            
             background = Image.open("/workspace/GoGoGnomes/static/images/Christmas/c4Blank.png").convert("RGBA")
             background.paste(foreground, (440,190), foreground)
             background.save("./media/new-image4.png")
-
+            
             background = Image.open("/workspace/GoGoGnomes/static/images/Christmas/c5Blank.png").convert("RGBA")
             background.paste(foreground, (440,190), foreground)
             background.save("./media/new-image5.png")
