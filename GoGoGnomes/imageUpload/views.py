1f381c98736e5f58731bd33ef670c82180561452
equati0n <tr14rc3@gmail.com>
2020-04-03 04:59:48 -0700

    Code Cleanup

--- GoGoGnomes/imageUpload/views.py
+++ GoGoGnomes/imageUpload/views.py
@@ -59,29 +59,7 @@ def imageUpload(request):
             itr += 1
 
         fs = FileSystemStorage()
-        fs.save(uploaded_image.name, uploaded_image)
-        '''if selection == "christmas":           
-            foreground = Image.open(uploaded_image).convert("RGBA").resize((950,550))
-            background = Image.open("/workspace/GoGoGnomes/static/images/Christmas/c1Blank.png").convert("RGBA")
-            background.paste(foreground, (440,190), foreground)            
-            background.save("./media/new-image1.png")
-            
-            background = Image.open("/workspace/GoGoGnomes/static/images/Christmas/c2Blank.png").convert("RGBA")
-            background.paste(foreground, (440,190), foreground)
-            background.save("./media/new-image2.png")
-            
-            background = Image.open("/workspace/GoGoGnomes/static/images/Christmas/c3Blank.png").convert("RGBA")
-            background.paste(foreground, (440,190), foreground)
-            background.save("./media/new-image3.png")
-            
-            background = Image.open("/workspace/GoGoGnomes/static/images/Christmas/c4Blank.png").convert("RGBA")
-            background.paste(foreground, (440,190), foreground)
-            background.save("./media/new-image4.png")
-            
-            background = Image.open("/workspace/GoGoGnomes/static/images/Christmas/c5Blank.png").convert("RGBA")
-            background.paste(foreground, (440,190), foreground)
-            background.save("./media/new-image5.png")
-        '''
+        fs.save(uploaded_image.name, uploaded_image)   
   
     return render(request, 'uploadpage.html')
 
