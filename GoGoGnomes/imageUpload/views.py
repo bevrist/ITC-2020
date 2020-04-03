e894791a3a57a80c8751f8737e8154f544c69751
wmentzer <willmentzer20@gmail.com>
2020-04-03 00:09:54 -0700

    Added holiday selection functionality

--- GoGoGnomes/imageUpload/views.py
+++ GoGoGnomes/imageUpload/views.py
@@ -4,22 +4,59 @@ from PIL import Image
 import base64
 import io
 
-from .models import Samplecards
+from .models import Samplecards, Holidays
 
 def imageUpload(request):
     if request.method == 'POST':
         selection = request.POST['selection']
-        sample = getImage()
-        theImage = io.BytesIO(sample.image)
-        print("DONE")
 
-        uploaded_image = request.FILES['img']
+        if selection == "Christmas":
+            holiday_id = 12
+        elif selection == "Thanksgiving":
+            holiday_id = 10
+        elif selection == "Mothers Day":
+            holiday_id = 4
+        elif selection == "Easter":
+            holiday_id = 5
+        elif selection == "Fourth of July":
+            holiday_id = 7
+        elif selection == "Fathers Day":
+            holiday_id = 6
+        elif selection == "Halloween":
+            holiday_id = 8
+        elif selection == "Valentines Day":
+            holiday_id = 2
+        elif selection == "Saint Patricks Day":
+            holiday_id = 3
+        elif selection == "New Years":
+            holiday_id = 1
+        elif selection == "Hanukkah":
+            holiday_id = 11
+        elif selection == "Kwanzaa":
+            holiday_id = 13
+        elif selection == "Diwali":
+            holiday_id = 9
+        else:
+            None
 
-        foreground = Image.open(uploaded_image).convert("RGBA")
-        background = Image.open(theImage).convert("RGBA")
+        itr = 0
 
-        background.paste(foreground, (440, 190), foreground)
-        background.save("./media/new-image1.png")
+        for x in Samplecards.objects.filter(holidayid=holiday_id):
+            sample = x
+            theImage = io.BytesIO(sample.image)
+            holidayID = sample.holidayid
+            print(holidayID)
+            print("DONE")
+
+            uploaded_image = request.FILES['img']
+
+            foreground = Image.open(uploaded_image).convert("RGBA")
+            background = Image.open(theImage).convert("RGBA")
+
+            background.paste(foreground, (440, 190), foreground)
+            print(itr)
+            background.save(("{0}"+str(itr)+".png").format("./media/image"))
+            itr += 1
 
         fs = FileSystemStorage()
         fs.save(uploaded_image.name, uploaded_image)
@@ -47,10 +84,6 @@ def imageUpload(request):
         '''
   
     return render(request, 'uploadpage.html')
-
-def getImage():
-    img = Samplecards.objects.get(image_id=1)
-    return img
 
 def upload(request):
     return render(request, 'uploadpage.html')
