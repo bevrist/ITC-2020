3858773168fd000c15837522e5ddc8b7aa1d7891
wmentzer <willmentzer20@gmail.com>
2020-04-03 23:18:55 -0700

    Added comments and screen to view combined images

--- GoGoGnomes/imageUpload/views.py
+++ GoGoGnomes/imageUpload/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from django.core.files.storage import FileSystemStorage
 from PIL import Image, ImageOps
-import base64
 import io
 
 from .models import Samplecards, Holidays
 
+# Main view for handling image upload and selection POSTs
 def imageUpload(request):
     if request.method == 'POST':
         selection = request.POST['selection']
 
+        # Assign holiday id for database calls depending on which holiday is selected
         if selection == "Christmas":
             holiday_id = 12
         elif selection == "Thanksgiving":
@@ -39,32 +39,36 @@ def imageUpload(request):
         else:
             None
 
+        # itr is used for saving each new sample
         itr = 0
 
-        for x in Samplecards.objects.filter(holidayid=holiday_id):
-            sample = x
+        # Iterate through all of the samples of the selected holiday
+        for sample in Samplecards.objects.filter(holidayid=holiday_id):
+            # Grab the image bytes
             theImage = io.BytesIO(sample.image)
-            holidayID = sample.holidayid
-            print(holidayID)
-            print("DONE")
 
-            uploaded_image = request.FILES['img']                       
-            foreground = Image.open(uploaded_image).convert("RGBA")
-            resizedForeground = foreground.resize((sample.resize_img_len, sample.resize_img_wid))
-            img_with_border = ImageOps.expand(resizedForeground,border=10,fill='white')
+            # Grab the user upload from POST request
+            uploaded_image = request.FILES['img']   
+
+            # Opens and formats user uploaded image using Pillow to be combined with sample background            
+            foreground = Image.open(uploaded_image).convert("RGBA").resize((sample.resize_img_len, sample.resize_img_wid))
+            
+            # Adds a styling border on the uploaded image
+            img_with_border = ImageOps.expand(foreground,border=10,fill='white')
+            
+            # Opens, formats, combines, and saves the background image combined with the user uploaded image
             background = Image.open(theImage).convert("RGBA")
             background.paste(img_with_border, (sample.user_img_pos_x, sample.user_img_pos_y), img_with_border)
-            print(itr)
             background.save(("{0}"+str(itr)+".png").format("./media/image"))
             itr += 1
 
-        fs = FileSystemStorage()
-        fs.save(uploaded_image.name, uploaded_image)   
-  
-    return render(request, 'uploadpage.html')
+    # Returns back to the user upload page
+    return render(request, 'imageUpload.html')
 
+# Returns a render back to the upload page when using the nav bar
 def upload(request):
     return render(request, 'uploadpage.html')
 
+# Returns a render back to the homepage when using the nav bar
 def index(request):
     return render(render, 'rootpage2.html')
